a790001aab99e2f6473547fd1e2138c6e51a5e65
feliciagirl <150937097+feliciagirl@users.noreply.github.com>
2023-11-15 16:17:09 -0300

    muuuuuusic
    eu amo musica por isso agora ja tem 18

--- hello.py
+++ hello.py
@@ -8,6 +8,20 @@ h = 'https://www.youtube.com/watch?v=uceKnghZ9NU'
 h5 = 'https://youtu.be/iN2O7vcwSDw'
 amigos = 'https://youtu.be/BjYUs_jJ8k8'
 a4 = 'https://youtu.be/ttkOAGuymr0?t=2'
+b45 = 'https://youtu.be/wi5P-54c2dI?t=9'
+cc = 'https://www.youtube.com/watch?v=yAmxbkKgf4g'
+dde = 'utube.com/watch?v=fvl3nm5BF9M'
+kk = 'https://www.youtube.com/watch?v=1ePsJtDibTE'
+opq = 'https://www.youtube.com/watch?v=0x9jXod9mPU'
+yyu = 'https://www.youtube.com/watch?v=usF7jRATWrs'
+ui = 'https://www.youtube.com/watch?v=xjLwV14UdFw'
+fe = 'https://www.youtube.com/watch?v=c5IPvpSS1gU'
+li = 'https://www.youtube.com/watch?v=KX5fBeQHycw'
+ci = 'https://www.youtube.com/watch?v=LIgC1EwZczk'
+agora = 'https://www.youtube.com/watch?v=I0U1goJh2Nk'
+depois = 'https://www.youtube.com/watch?v=7GiXiAKVTG4'
+laura = 'https://www.youtube.com/watch?v=bGZ-ay4Rc0w'
+
 st.subheader(':standing_person: strangers ')
 st.video(happy)
 
@@ -28,3 +42,42 @@ st.video(amigos)
 
 st.subheader(':candle: mix 5')
 st.video(a4)
+
+st.subheader(:100: mix 6')
+st.video(b45)
+
+st.subheader(':car: mix 7')
+st.video(cc)
+
+st.subheader(':bird: mix 8')
+st.video(dde)
+
+st.subheader(':coffee: mix 9')
+st.video(kk)
+
+st.subheader(':anchor: mix 10')
+st.video(opq)
+
+st.subheader(':white_check_mark: mix 11')
+st.video(yyu)
+
+st.subheader(':sparkles: mix 12')
+st.video(ui)
+
+st.subheader(':question: mix 13')
+st.video(fe)
+
+st.subheader(':grey_exclamation: mix 14')
+st.video(li)
+
+st.subheader(':heavy_plus_sign: mix 15')
+st.video(ci)
+
+st.subheader(':hash: mix 16')
+st.video(agora)
+
+st.subheader(':zero: mix 17')
+st.video(depois)
+
+st.subheader(':six: mix 18')
+st.video(laura)
