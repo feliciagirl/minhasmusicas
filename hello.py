a927d2dcc47bbbac1b3ae64e7e142eb0b1787e0b
feliciagirl <150937097+feliciagirl@users.noreply.github.com>
2023-11-15 12:37:36 -0300

    novas musicas
    novas musicas adicionadas e botao retirado

--- hello.py
+++ hello.py
@@ -4,13 +4,13 @@ st.header('minhas musicas')
 happy = 'https://www.youtube.com/watch?v=RnyPNdBWwVw'
 ideo = 'https://www.youtube.com/watch?v=KyDov-QJFGE'
 oi = 'https://www.youtube.com/watch?v=EBWzgb1Xeis'
+hi = 'https://www.youtube.com/watch?v=uceKnghZ9NU'
 
-if st.button('quero ver mais'):
-    st.subheader(':standing_person: strangers ')
-    st.video(happy)
-    st.subheader(':radio: stereo hearts ')
-    st.video(ideo)
-    st.subheader(':eyes: mix ')
-    st.video(oi)
-else:
-    st.write('(para ver mais musicas, clique no botao acima:)')
+st.subheader(':standing_person: strangers ')
+st.video(happy)
+st.subheader(':radio: stereo hearts ')
+st.video(ideo)
+st.subheader(':eyes: mix ')
+st.video(oi)
+st.subheader(':wave: mix 2 ')
+st.video(hi,start_time=194)
