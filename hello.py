d88359fe6f6d14fff32423e8100ff8db39cc905c
feliciagirl <150937097+feliciagirl@users.noreply.github.com>
2023-11-15 12:58:39 -0300

    good music
    adicionamos muitas musicas

--- hello.py
+++ hello.py
@@ -4,13 +4,27 @@ st.header('minhas musicas')
 happy = 'https://www.youtube.com/watch?v=RnyPNdBWwVw'
 ideo = 'https://www.youtube.com/watch?v=KyDov-QJFGE'
 oi = 'https://www.youtube.com/watch?v=EBWzgb1Xeis'
-hi = 'https://www.youtube.com/watch?v=uceKnghZ9NU'
-
+h = 'https://www.youtube.com/watch?v=uceKnghZ9NU'
+h5 = 'https://youtu.be/iN2O7vcwSDw'
+amigos = 'https://youtu.be/BjYUs_jJ8k8'
+a4 = 'https://youtu.be/ttkOAGuymr0?t=2'
 st.subheader(':standing_person: strangers ')
 st.video(happy)
+
 st.subheader(':radio: stereo hearts ')
 st.video(ideo)
+
 st.subheader(':eyes: mix ')
 st.video(oi)
+
 st.subheader(':wave: mix 2 ')
-st.video(hi,start_time=194)
+st.video(h)
+
+st.subheader(':smile: mix 3 ')
+st.video(h5)
+
+st.subheader(':dark_sunglasses: mix 4 ')
+st.video(amigos)
+
+st.subheader(':candle: mix 5')
+st.video(a4)
